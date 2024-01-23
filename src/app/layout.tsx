59ade677b75a5c39bef4ec4f34515685df830b29
daviiddd18 <davidespinossanz@gmail.com>
2024-01-23 17:48:16 +0100

    Actualización @ por ..

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { cn } from "@/lib/utils";
-import Navbar from "@/components/NavBar";
+import { cn } from "../lib/utils";
+import Navbar from "../components/NavBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import MaxWidthWrapper from "@/components/MaxWidthWrapper";
-import { Button, buttonVariants } from "@/components/ui/button";
+import MaxWidthWrapper from "../components/MaxWidthWrapper";
+import { Button, buttonVariants } from "../components/ui/button";
 import { ArrowDownToLine, CheckCircle, LeafyGreen } from "lucide-react";
 import Link from "next/link";
 
