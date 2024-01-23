e208a148c9f81b4003c3ec9e0fc7c8265f76d4eb
daviiddd18 <davidespinossanz@gmail.com>
2024-01-23 18:06:24 +0100

    Speed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import Navbar from "@/components/Navbar";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
         <main className="relative flex flex-col min-h-screen">
           <Navbar/>
           <div className="flex-grow flex-1">{children}</div>
+          <SpeedInsights/>
         </main>
       </body>
     </html>
